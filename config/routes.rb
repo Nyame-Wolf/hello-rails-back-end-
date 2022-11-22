2dbdb479ca8f44dd60daa01c85eeba07b2e144f3
Nyame-Wolf <mumenyam@gmail.com>
2022-11-23 00:51:40 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'greetings/index'
+   root "greetings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
